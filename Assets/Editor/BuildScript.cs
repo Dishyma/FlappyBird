a3de8b43cceb778f4fed44c4c7bf7d2ce7eaf05e
dishyma <marojas4126@gmail.com>
2024-10-22 20:56:08 -0500

    Fix: modified dockerfile, and add license

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,13 +1,19 @@
 using UnityEditor;
 using UnityEngine;
+using System.Linq;
 
 public class BuildScript
 {
     [MenuItem("Build/Build Linux")]
     public static void BuildLinux()
     {
+        string[] scenes = EditorBuildSettings.scenes
+            .Where(scene => scene.enabled)
+            .Select(scene => scene.path)
+            .ToArray();
+
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = EditorBuildSettings.scenes;
+        buildPlayerOptions.scenes = scenes;
         buildPlayerOptions.locationPathName = "Builds/LinuxBuild/Game.x86_64";
         buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
         buildPlayerOptions.options = BuildOptions.None;
@@ -20,8 +26,13 @@ public class BuildScript
     [MenuItem("Build/Build Windows")]
     public static void BuildWindows()
     {
+        string[] scenes = EditorBuildSettings.scenes
+            .Where(scene => scene.enabled)
+            .Select(scene => scene.path)
+            .ToArray();
+
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = EditorBuildSettings.scenes;
+        buildPlayerOptions.scenes = scenes;
         buildPlayerOptions.locationPathName = "Builds/WindowsBuild/Game.exe";
         buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
         buildPlayerOptions.options = BuildOptions.None;
--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
-# Imagen base con Unity
-FROM gableroux/unity3d:2022.3.16f1-linux-il2cpp
-
-# Argumentos para las credenciales de Unity (necesarias para la activación)
-ARG UNITY_USERNAME
-ARG UNITY_PASSWORD
-ARG UNITY_SERIAL
+# Imagen base con Unity (usando una versión disponible)
+FROM gableroux/unity3d:2020.1.1f1-linux-il2cpp
 
 # Directorio de trabajo
 WORKDIR /app
@@ -17,16 +12,23 @@ RUN apt-get update && apt-get install -y \
     xvfb \
     && rm -rf /var/lib/apt/lists/*
 
-# Script para construir el proyecto
+# Script para activar Unity y construir el proyecto
 RUN echo '#!/bin/bash\n\
+# Activar Unity Personal\n\
+xvfb-run --auto-servernum --server-args="-screen 0 640x480x24" \
+/opt/Unity/Editor/Unity \
+    -batchmode \
+    -nographics \
+    -username "$UNITY_USERNAME" \
+    -password "$UNITY_PASSWORD" \
+    -quit \
+    || true\n\
+# Construir el proyecto\n\
 xvfb-run --auto-servernum --server-args="-screen 0 640x480x24" \
 /opt/Unity/Editor/Unity \
     -quit \
     -batchmode \
     -nographics \
-    -username "$UNITY_USERNAME" \
-    -password "$UNITY_PASSWORD" \
-    -serial "$UNITY_SERIAL" \
     -projectPath /app \
     -executeMethod BuildScript.BuildLinux \
     -logFile /dev/stdout' > /app/build.sh \
